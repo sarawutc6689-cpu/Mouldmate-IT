4810c175f11f8dda7b219b87ebd0dd5091e7b307
sarawutc6689-cpu <sarawut.c6689@gmail.com>
2026-09-24 14:51:13 +0700

    Update config.js

--- Form/012/config.js
+++ Form/012/config.js
@@ -1,6 +1,6 @@
 // ===== ใส่ค่าจาก Supabase: Project Settings > API =====
-const SUPABASE_URL = 'https://YOUR-PROJECT-ID.supabase.co';
-const SUPABASE_KEY = 'YOUR-ANON-OR-PUBLISHABLE-KEY';
+const SUPABASE_URL = 'https://fnbkcgkuykdabbensvif.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZuYmtjZ2t1eWtkYWJiZW5zdmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzMDE0MTQsImV4cCI6MjEwNDg3NzQxNH0.L6x3nqAy2rqcxdtzyTs4MHLPubtBqD3Tnk9e97EfEJE';
 // ======================================================
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
