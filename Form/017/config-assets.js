0012ecfea7d0187eba98329ca10673b988eeac54
sarawutc6689-cpu <sarawut.c6689@gmail.com>
2026-09-26 11:33:12 +0700

    Update config-assets.js

--- Form/017/config-assets.js
+++ Form/017/config-assets.js
@@ -1,6 +1,6 @@
 // ===== โปรเจค B: โปรเจคที่มีตาราง assets อยู่แล้ว =====
-const SUPABASE_B_URL = 'https://YOUR-PROJECT-B-ID.supabase.co';
-const SUPABASE_B_KEY = 'YOUR-PROJECT-B-ANON-OR-PUBLISHABLE-KEY';
+const SUPABASE_B_URL = 'https://fuarmmweqxxhczavkvqp.supabase.co';
+const SUPABASE_B_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1YXJtbXdlcXh4aGN6YXZrdnFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyMjgzMTcsImV4cCI6MjEwMzgwNDMxN30.n7s4gKOkCvaOSpFSf9FGmWsqi7eAzFe28c_oJWy8NSE';
 // ==============================================================
 
 const sbB = supabase.createClient(SUPABASE_B_URL, SUPABASE_B_KEY);
